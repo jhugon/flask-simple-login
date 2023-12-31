8e844704884dc3bb626ee9748816beb8ff80e98e
Justin Hugon <jhugon@users.noreply.github.com>
2023-12-30 19:48:40 -0600

    Create db.py

---
+++ flask_simple_login/db.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+class DBUser(db.Model):
+    username: Mapped[str] = mapped_column(primary_key=True)
+    passwordhash: Mapped[str]
