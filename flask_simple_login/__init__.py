9adb5248d4485a0a7ade1a57d3f3009add810e01
Justin Hugon <jhugon@users.noreply.github.com>
2024-01-01 11:31:55 -0600

    Update __init__.py: add setup_auth to imports

--- flask_simple_login/__init__.py
+++ flask_simple_login/__init__.py
@@ -4,5 +4,6 @@ The classes and methods you use externally are in here
 from .users import User
 from .web import auth
 from .web import login_manager
+from .web import setup_auth
 from flask_login import login_required
 from flask_login import current_user
